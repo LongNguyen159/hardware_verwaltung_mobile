8d5b4e7bcb2cc607a39f6f71d087f4b2df3ae2a3
long <nguyenthanhlong@morenergy.net>
2024-04-03 11:31:04 +0200

    feat: reusable input for title bar

--- frontend_service/src/app/shared/components/title-bar/title-bar.component.ts
+++ frontend_service/src/app/shared/components/title-bar/title-bar.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-title-bar',
@@ -7,4 +9,15 @@ import { Component, Input } from '@angular/core';
 })
 export class TitleBarComponent {
   @Input() title: string = ''
+  @Input() colorBackground: boolean = false
+  @Input() enableBackButton: boolean = false
+
+  constructor(
+    private router: Router,
+    private _location: Location
+  ) {}
+
+  onBackButtonClick() {
+    this._location.back()
+  }
 }
