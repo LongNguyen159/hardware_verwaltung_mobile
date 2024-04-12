6099038781aa7e19b8b8ec518eadfa2cc4089af4
long <nguyenthanhlong@morenergy.net>
2024-04-12 19:14:52 +0200

    feat: device service

--- mobile_version/hardware_verwaltung_client/src/app/feature/device-view/pages/device-view-page/device-view-page.component.ts
+++ mobile_version/hardware_verwaltung_client/src/app/feature/device-view/pages/device-view-page/device-view-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { take } from 'rxjs';
 import { TitleBarComponent } from 'src/app/shared/components/title-bar/title-bar.component';
+import { DeviceMetaData } from 'src/app/shared/models/shared-models';
+import { SharedService } from 'src/app/shared/services/shared.service';
+
 @Component({
   selector: 'app-device-view-page',
   templateUrl: './device-view-page.component.html',
@@ -9,9 +13,14 @@ import { TitleBarComponent } from 'src/app/shared/components/title-bar/title-bar
   imports: [IonHeader, TitleBarComponent],
 })
 export class DeviceViewPageComponent  implements OnInit {
+  allDevices: DeviceMetaData[] = []
+  constructor(private sharedService: SharedService) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.sharedService.getAllItems().pipe(take(1)).subscribe(allItems => {
+      this.allDevices = allItems
+      console.log(allItems)
+    })
+  }
 
 }
---
+++ mobile_version/hardware_verwaltung_client/src/app/shared/services/shared.service.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { DeviceMetaData, ImageResponse, NewDeviceData, ProductType, RoomInterface } from '../models/shared-models';
+import { Observable, map, of, timer} from 'rxjs';
+import { switchMap } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  /** TODO: Change API endpoint Host in production. Not 'localhost' anymore, but the IP
+   * of server where it's been hosted.
+   */
+  apiBaseHostUrl: string = 'http://localhost:8000'
+  apiEndpoint: string = `${this.apiBaseHostUrl}/api/v1`
+  imageId: number
+  unixTimeValue: number
+
+  constructor(private http: HttpClient) { }
+
+  createNewDevice(newDeviceData: NewDeviceData[]) {
+    return this.http.post(`${this.apiEndpoint}/items-new/`, newDeviceData)
+  }
+
+  /** Get all Items, polling to update the changes from DB */
+  getAllItems() {
+    return timer(1, 10000).pipe(
+      // Use switchMap to switch to a new observable each time interval emits a value
+      switchMap(() => this.http.get<DeviceMetaData[]>(`${this.apiEndpoint}/items-all/`))
+    )
+  }
+
+  /** Get 1 item infos, polling to reflect changes in DB */
+  getItemById(itemId: number): Observable<DeviceMetaData> {
+    return timer(1, 10000).pipe(
+      // Use switchMap to switch to a new observable each time interval emits a value
+      switchMap(() => this.http.get<DeviceMetaData>(`${this.apiEndpoint}/item-details/${itemId}/`)),
+    )
+  }
+
+  updateItemNotes(itemId: number, notes: string) {
+    const patchData = {
+      annotation: notes
+    }
+    return this.http.patch(`${this.apiEndpoint}/item/id/${itemId}/`, patchData)
+  }
+
+  deleteItemById(itemId: number) {
+    return this.http.delete(`${this.apiEndpoint}/item/id/${itemId}/`)
+  }
+
+  getAllProductTypes() {
+    return this.http.get<ProductType[]>(`${this.apiEndpoint}/product-type/`)
+  }
+
+  getAllRooms() {
+    return timer(1, 10000).pipe(
+      switchMap(() => this.http.get<RoomInterface[]>(`${this.apiEndpoint}/room/`))
+    )
+  }
+
+  getOneRoom(roomId: number) {
+    return this.http.get<RoomInterface>(`${this.apiEndpoint}/room/id/${roomId}/`)
+  }
+
+  createNewRoom(roomNumber: string) {
+    /** POST JSON to create new room */
+    const postData = {
+      room_number: roomNumber
+    }
+    return this.http.post(`${this.apiEndpoint}/room/`, postData)
+  }
+
+  deleteRoom(roomId: number) {
+    return this.http.delete(`${this.apiEndpoint}/room/id/${roomId}/`)
+  }
+
+  uploadDeviceImageToServer(formData: FormData, deviceId: number) {
+    return this.http.post(`${this.apiEndpoint}/items/${deviceId}/image/`, formData)
+  }
+
+  /** Get image (blob) of one device */
+  getImageOfDevice(deviceId: number): Observable<Blob | null> {
+    return this.getImageInfos(deviceId).pipe(
+      switchMap(response => {
+        if (response.length !== 0) {
+
+          return this.getImageBlob(response[0].image)
+        } else {
+          return of(null)
+        }
+      })
+    )
+  }
+  /** Get all images URL associated with one device ID */
+  getImageInfos(deviceId: number): Observable<ImageResponse[]> {
+    return this.http.get<ImageResponse[]>(`${this.apiEndpoint}/items/${deviceId}/image/`)
+  }
+
+  getImageBlob(imageUrl: string): Observable<Blob> {
+    return this.http.get(`${this.apiBaseHostUrl}${imageUrl}`, { responseType: 'blob' })
+  }
+
+  clearImage(deviceId: number) {
+    return this.http.delete(`${this.apiEndpoint}/items/${deviceId}/image/`)
+  }
+}
