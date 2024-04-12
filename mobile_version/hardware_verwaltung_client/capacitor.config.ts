8789c9450500dafbc17fed7aa51512c7decd1ee8
long <nguyenthanhlong@morenergy.net>
2024-04-12 16:38:52 +0200

    update angular

---
+++ mobile_version/hardware_verwaltung_client/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hardware_verwaltung_client',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
