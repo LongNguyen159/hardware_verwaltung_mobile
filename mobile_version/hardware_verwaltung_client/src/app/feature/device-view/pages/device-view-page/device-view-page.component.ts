4b67f6a345db473f6d0a86cab1be0f7a60a869bc
long <nguyenthanhlong@morenergy.net>
2024-04-14 21:16:45 +0200

    feat: polling

--- mobile_version/hardware_verwaltung_client/src/app/feature/device-view/pages/device-view-page/device-view-page.component.ts
+++ mobile_version/hardware_verwaltung_client/src/app/feature/device-view/pages/device-view-page/device-view-page.component.ts
@@ -2,7 +2,8 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonSkeletonText, IonList, IonItem, IonInfiniteScroll, IonLabel, IonAlert, IonInfiniteScrollContent, IonLoading, IonButton, IonBackButton, IonButtons } from '@ionic/angular/standalone';
-import { take } from 'rxjs';
+import { take, takeUntil } from 'rxjs';
+import { BaseComponent } from 'src/app/shared/components/base/base.component';
 import { TitleBarComponent } from 'src/app/shared/components/title-bar/title-bar.component';
 import { DeviceMetaData } from 'src/app/shared/models/shared-models';
 import { PlatformService } from 'src/app/shared/services/platform.service';
@@ -34,14 +35,15 @@ import { SharedService } from 'src/app/shared/services/shared.service';
     IonBackButton
   ],
 })
-export class DeviceViewPageComponent  implements OnInit {
-  allDevices: DeviceMetaData[] = []
-  constructor(private sharedService: SharedService, public platformService: PlatformService) { }
+export class DeviceViewPageComponent extends BaseComponent implements OnInit {
+  allAvailableDevices: DeviceMetaData[] = []
+  constructor(private sharedService: SharedService, public platformService: PlatformService) {
+    super()
+  }
 
   ngOnInit() {
-    this.sharedService.getAllItems().pipe(take(1)).subscribe(allItems => {
-      this.allDevices = allItems
-      console.log(allItems)
+    this.sharedService.getAllItems().pipe(takeUntil(this.componentDestroyed$)).subscribe(allItems => {
+      this.allAvailableDevices = allItems.filter(item => !item.borrowed_by_user_id)
     })
   }
 
