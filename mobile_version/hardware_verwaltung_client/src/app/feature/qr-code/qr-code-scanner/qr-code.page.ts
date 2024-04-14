2aafb057f6177ff12a6745267be73bae78ab4748
long <nguyenthanhlong@morenergy.net>
2024-04-14 17:50:09 +0200

    feat: qr home page

--- mobile_version/hardware_verwaltung_client/src/app/feature/qr-code/qr-code-scanner/qr-code.page.ts
+++ mobile_version/hardware_verwaltung_client/src/app/feature/qr-code/qr-code-scanner/qr-code.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../../explore-container/explore-container.component';
 import { TitleBarComponent } from '../../../shared/components/title-bar/title-bar.component';
 import { addIcons } from 'ionicons';
-import { scan } from 'ionicons/icons';
+import { chevronForward, scan } from 'ionicons/icons';
 import { QrCodeService } from '../service/qr-code.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-qr-code',
@@ -12,13 +13,14 @@ import { QrCodeService } from '../service/qr-code.service';
   styleUrls: ['qr-code.page.scss'],
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, TitleBarComponent,
-    IonFab, IonFabButton, IonIcon
+    IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle,
+    RouterModule
   ]
 })
 export class QrCodeScanner {
 
   constructor(public qrCodeService: QrCodeService) {
-    addIcons({scan})
+    addIcons({scan, chevronForward})
   }
 
   scan() {
