3fb7d52505be10dc56a09febdeed5313efd32ede
long <nguyenthanhlong@morenergy.net>
2024-04-14 12:24:08 +0200

    refactor: remove unused code

--- mobile_version/hardware_verwaltung_client/src/app/feature/room-view/pages/room-view-page/room-view.page.ts
+++ mobile_version/hardware_verwaltung_client/src/app/feature/room-view/pages/room-view-page/room-view.page.ts
@@ -38,11 +38,11 @@ export class RoomOverviewPageComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getAllRoomsData()
-    this.getAvailableItemsCount()
+    this.getAllRooms()
+    this.getAllItems()
   }
 
-  getAllRoomsData() {
+  getAllRooms() {
     this.sharedService.getAllRooms().pipe(takeUntil(this.componentDestroyed$)).subscribe(
       (rooms: RoomInterface[]) => {
         this.allRooms = rooms
@@ -50,34 +50,20 @@ export class RoomOverviewPageComponent extends BaseComponent implements OnInit {
     )
   }
 
-  getAvailableItemsCount() {
+  getAllItems() {
     this.sharedService.getAllItems().pipe(takeUntil(this.componentDestroyed$)).subscribe(allItems => {
-      /** Get items of specific room */
-      this.allItems = allItems      
-      // this.allItemsOfRoom = allItems.filter(item => item.location === this.roomDetails.room_number)
-      /** Filter out which items has not been borrowed => hence 'available items' */
-      // this.availableItems = this.allItemsOfRoom.filter(item => !item.borrowed_by_user_id)
+      this.allItems = allItems
     })
   }
 
   getAvailableItemsCountForRoom(roomId: number): number {
     const room = this.allRooms.find(room => room.id === roomId)
     if (room) {
-      const itemsOfRoom = this.allItems.filter(item => item.location === room.room_number);
-      const availableItems = itemsOfRoom.filter(item => !item.borrowed_by_user_id);
+      const itemsOfRoom = this.allItems.filter(item => item.location === room.room_number)
+      const availableItems = itemsOfRoom.filter(item => !item.borrowed_by_user_id)
       return availableItems.length
     } else {
       return 0
     }
-  }
-
-
-  getRoomDetails(id: number) {
-    this.sharedService.getOneRoom(id).pipe(take(1)).subscribe((room: RoomInterface) => {
-      this.roomDetails = room
-
-      /** Get all items of that room */
-      this.getAvailableItemsCount()
-    })
   }
 }
