662107ca9774115c65b45f68e0eaec45b1ceb51a
long <nguyenthanhlong@morenergy.net>
2024-05-04 11:23:44 +0200

    placeholder for future uses

--- frontend/src/app/shared/components/title-bar/title-bar.component.ts
+++ frontend/src/app/shared/components/title-bar/title-bar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
-import { IonBackButton, IonButtons, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { PlatformService } from '../../services/platform.service';
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
@@ -12,11 +13,15 @@ import { IonBackButton, IonButtons, IonHeader, IonTitle, IonToolbar } from '@ion
     IonToolbar,
     IonButtons,
     IonBackButton,
-    CommonModule
+    CommonModule,
+    IonContent,
+    
   ]
 })
 export class TitleBarComponent {
   @Input() title: string = ''
   @Input() enableBackButton: boolean = false
   @Input() backLocation: string
+
+  constructor(public platformService: PlatformService) {}
 }
