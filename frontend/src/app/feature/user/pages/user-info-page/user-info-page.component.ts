2e625e1ec57114b719779b1ff5b7d7c0c039efcc
long <nguyenthanhlong@morenergy.net>
2024-05-04 11:38:34 +0200

    ops: install tailwind

--- frontend/src/app/feature/user/pages/user-info-page/user-info-page.component.ts
+++ frontend/src/app/feature/user/pages/user-info-page/user-info-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { IonIcon } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonAvatar } from '@ionic/angular/standalone';
+import { PlatformService } from 'src/app/shared/services/platform.service';
 
 @Component({
   selector: 'app-user-info-page',
@@ -7,11 +9,19 @@ import { IonIcon } from '@ionic/angular/standalone';
   styleUrls: ['./user-info-page.component.scss'],
   standalone: true,
   imports: [
-    IonIcon
+    IonIcon,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonContent,
+    CommonModule,
+    IonAvatar
   ]
 })
 export class UserInfoPageComponent  implements OnInit {
-
+  platformService = inject(PlatformService)
   constructor() { }
 
   ngOnInit() {}
