8f808d962566190652dbdfb2502e6b68187d07ca
long <nguyenthanhlong@morenergy.net>
2024-05-12 22:00:18 +0200

    feat: image placeholder, invert as mode change

--- frontend/src/app/feature/user/pages/user-info-page/user-info-page.component.ts
+++ frontend/src/app/feature/user/pages/user-info-page/user-info-page.component.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonAvatar, IonButton, IonCard, IonCardContent } from '@ionic/angular/standalone';
-import { take } from 'rxjs';
+import { take, takeUntil } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/components/base/base.component';
 import { User } from 'src/app/shared/models/shared-models';
+import { ColorModeService } from 'src/app/shared/services/color-mode.service';
 import { PlatformService } from 'src/app/shared/services/platform.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -30,10 +31,11 @@ import { UserService } from 'src/app/shared/services/user.service';
 export class UserInfoPageComponent extends BaseComponent implements OnInit {
   platformService = inject(PlatformService)
   userService = inject(UserService)
+  colorModeService = inject(ColorModeService)
 
   userId: number
-
   user: User
+  colorTheme: string = ''
 
   constructor() { 
     super()
@@ -43,6 +45,15 @@ export class UserInfoPageComponent extends BaseComponent implements OnInit {
     this.userId = this.userService.testUserId
 
     this.getUserInfos()
+
+    /** Get color theme mode: dark or light. This is for the inverted image place holder of the avatar */
+    this.getColorMode()
+  }
+
+  getColorMode() {
+    this.colorModeService.getUserUiMode().pipe(takeUntil(this.componentDestroyed$)).subscribe( (theme: string) => {
+      this.colorTheme = theme
+    })
   }
 
   getUserInfos() {
