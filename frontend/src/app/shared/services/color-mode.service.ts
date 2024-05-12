a8d1a819ba42620fdf48bc42b4018863e28137d0
long <nguyenthanhlong@morenergy.net>
2024-05-12 21:54:58 +0200

    refactor: rename variable, change them to private

--- frontend/src/app/shared/services/color-mode.service.ts
+++ frontend/src/app/shared/services/color-mode.service.ts
@@ -5,39 +5,43 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ColorModeService {
-  public darkMode$ = new BehaviorSubject<boolean>(false);
+  public darkMode$ = new BehaviorSubject<boolean>(false)
 
   // Check user preference for light/dark theme
-  preferColorScheme: MediaQueryList
-  UiMode: string = ''
-  UiModeSubject = new BehaviorSubject<string>(this.UiMode)
+  private _preferColorScheme: MediaQueryList
+  private _UiMode: string = ''
+  private _UiModeSubject = new BehaviorSubject<string>(this._UiMode)
 
   darkModeListener: (event: MediaQueryListEvent) => void;
+
   constructor() { 
-    this.listenToUiModes()
+    this._listenToUiModes()
   }
-  listenToUiModes() {
-    this.preferColorScheme = window.matchMedia('(prefers-color-scheme: dark)')
-    const initialMode = this.preferColorScheme.matches ? "dark" : "light"
-    this.UiMode = initialMode
-    this.UiModeSubject.next(this.UiMode)
+  private _listenToUiModes() {
+    this._preferColorScheme = window.matchMedia('(prefers-color-scheme: dark)')
+    const initialMode = this._preferColorScheme.matches ? "dark" : "light"
+    this._UiMode = initialMode
+    this._UiModeSubject.next(this._UiMode)
 
     this.darkModeListener = (event: MediaQueryListEvent) => {
       const newColorScheme = event.matches ? "dark" : "light";
-      this.UiMode = newColorScheme
-      this.UiModeSubject.next(this.UiMode)
+      this._UiMode = newColorScheme
+      this._UiModeSubject.next(this._UiMode)
 
 
     }
-    this.preferColorScheme.addEventListener('change', this.darkModeListener);
-    return this.UiMode
+    this._preferColorScheme.addEventListener('change', this.darkModeListener);
+    return this._UiMode
   }
 
+  /** Get UI mode of system. Emits a value everytime user changes system mode (dark - light).
+   * Observable string: 'dark', 'light'
+   */
   getUserUiMode() {
-    return this.UiModeSubject.asObservable()
+    return this._UiModeSubject.asObservable()
   }
 
   removeSubscriptionDarkmodeLightmode() {
-    this.preferColorScheme.removeEventListener('change', this.darkModeListener)
+    this._preferColorScheme.removeEventListener('change', this.darkModeListener)
   }
 }
