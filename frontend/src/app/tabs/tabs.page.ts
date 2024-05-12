a68409db27ecb7d20232b5940a255b2e475519b6
long <nguyenthanhlong@morenergy.net>
2024-05-12 22:36:53 +0200

    feat: highlight tabs on active or on touch

--- frontend/src/app/tabs/tabs.page.ts
+++ frontend/src/app/tabs/tabs.page.ts
@@ -27,6 +27,8 @@ export class TabsPage extends BaseComponent implements OnInit {
   selectedTab: string = ''
   tabNameExtractFromUrl = ''
 
+  timeOutId: any
+
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor(private router: Router) {
@@ -43,9 +45,11 @@ export class TabsPage extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    /** Highlight the active tab by the link on init */
     this._highlightActiveTab(this.router.url)
   }
 
+  /** Event triggered every time router changes the link (navigating). */
   private _listenToRouterChanges() {
     this.router.events.pipe(takeUntil(this.componentDestroyed$)).subscribe((event: any) => {
       if (event instanceof NavigationEnd) {
@@ -53,15 +57,41 @@ export class TabsPage extends BaseComponent implements OnInit {
       }
     })
   }
-
+  /** Extract the router url and assign the highlighted tab accordingly */
   private _highlightActiveTab(url: string) {
     const parts = url.split('/')
-      const tabName = parts[parts.length - 1]
-      this.tabNameExtractFromUrl = tabName
-      this.selectedTab = tabName  
+    const tabName = parts[parts.length - 1]
+    this.tabNameExtractFromUrl = tabName
+    this.selectedTab = tabName
+  }
+  /** Event triggered on tab button touch, there is a touch timeout.
+   * If touch and holding for too long, the touch will be reset.
+   * The touch tab will be set again to the url (active tab)
+   */
+  onTabButtonsTouch(tabName: string) {
+    const touchTimeout = 700
+    /** Assign selected tab to be the touched tab. After timeout, reset the selected tab
+     * to be the current active tab. (determined by url)
+     */
+    this.selectedTab = tabName
+
+    /** Clear timeout before starting a timeout to avoid duplicate timers */
+    if (this.timeOutId) {
+      clearTimeout(this.timeOutId)
+    }
+
+    // Set a timeout to reset the highlightedTab after the specified duration
+    this.timeOutId = setTimeout(() => {
+      this.selectedTab = this.tabNameExtractFromUrl
+    }, touchTimeout)
   }
 
-  async onQrClick() {    
+  /** Assign selected tab to be the active tab, determined by the url */
+  onTabButtonsClick() {
+    this.selectedTab = this.tabNameExtractFromUrl
+  }
+
+  onQrClick() {
     this.qrCodeService.scanLendDevice()
   }
 }
